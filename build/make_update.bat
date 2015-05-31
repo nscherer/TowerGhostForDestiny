88a565034b012ec7de7df2bac9780829dcc350c7
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-31 19:17:24 -0400

    ...

--- build/make_update.bat
+++ build/make_update.bat
@@ -1,4 +1,4 @@
 del www.zip
 cd ..\www
-"C:\Program Files\WinRAR\WinRAR.exe" a -r -x.svn -x.git -xconfig.xml -xassets -xdata\common -xdata\img -xres -xjs -x*.html ..\build\www.zip .
+"C:\Program Files\WinRAR\WinRAR.exe" a -r -x.svn -x.git -xconfig.xml -xassets -xdata\common -xdata\img -xres -xjs ..\build\www.zip .
 cd ..\build
